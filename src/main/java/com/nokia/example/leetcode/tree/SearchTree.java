8bc432a9e5f71ccfb8a137950e5bfa94c4f5fc1c
yao_yinglong <yao_yinglong@icekredit.com>
2020-08-03 09:26:17 +0800

    feat：补充一揽子leetcode解题3

--- src/main/java/com/nokia/example/leetcode/tree/SearchTree.java
+++ src/main/java/com/nokia/example/leetcode/tree/SearchTree.java
@@ -11,6 +11,7 @@ public class SearchTree {
      * BiNode
      */
     TreeNode cursor;
+
     public TreeNode increasingBST(TreeNode root) {
         TreeNode ans = new TreeNode(0);
         cursor = ans;
@@ -37,6 +38,7 @@ public class SearchTree {
      * 使用反序中序遍历
      */
     int sum = 0;
+
     public TreeNode convertBST(TreeNode root) {
         if (root == null) {
             return null;
@@ -46,5 +48,35 @@ public class SearchTree {
         root.val = sum;
         convertBST(root.left);
         return root;
+    }
+
+    /**
+     * 687
+     * 最长同值路径
+     *
+     * @param root
+     * @return
+     */
+    public int longest = 0;
+    public int longestUnivaluePath(TreeNode root) {
+        traversal(root);
+        return longest;
+    }
+
+    public int traversal(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = traversal(root.left);
+        int right = traversal(root.right);
+        int arrowLeft = 0, arrowRight = 0;
+        if (root.left != null && root.left.val == root.val) {
+            arrowLeft += left + 1;
+        }
+        if (root.right != null && root.right.val == root.val) {
+            arrowRight += right + 1;
+        }
+        longest = Math.max(longest, arrowLeft + arrowRight);
+        return Math.max(arrowLeft, arrowRight);
     }
 }
