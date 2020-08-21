59051b6efd5d39b950318d9cd500262f00de2417
yao_yinglong <yao_yinglong@icekredit.com>
2020-08-21 13:19:52 +0800

    feat：补充一揽子leetcode解题105

--- src/main/java/com/nokia/example/leetcode/tree/TreeTraversal.java
+++ src/main/java/com/nokia/example/leetcode/tree/TreeTraversal.java
@@ -476,4 +476,50 @@ public class TreeTraversal {
         }
         return root;
     }
+
+    /**
+     * 951
+     * 翻转等价二叉树
+     *
+     * @param root1
+     * @param root2
+     * @return
+     */
+    public boolean flipEquiv(TreeNode root1, TreeNode root2) {
+        if (root1 == null && root2 == null) {
+            return true;
+        }
+        if (root1 == null || root2 == null || root1.val != root2.val) {
+            return false;
+        }
+        boolean left = flipEquiv(root1.left, root2.right) || flipEquiv(root1.left, root2.left);
+        if (!left) {
+            return false;
+        }
+        return flipEquiv(root1.right, root2.left) || flipEquiv(root1.right, root2.right);
+    }
+
+    /**
+     * 129
+     * 求根到叶子节点数字之和
+     *
+     * @param root
+     * @return
+     */
+    public int sumNumbers(TreeNode root) {
+        return sumNumbersDfs(root, 0);
+    }
+
+    public int sumNumbersDfs(TreeNode root, int k) {
+        if (root == null) {
+            return 0;
+        }
+        k = k * 10 + root.val;
+        int left = sumNumbersDfs(root.left, k);
+        int right = sumNumbersDfs(root.right, k);
+        if (root.left == null && root.right == null) {
+            return k;
+        }
+        return left + right;
+    }
 }
