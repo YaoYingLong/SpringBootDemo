8ec80b62c7ecd86d65ad0da01fe9d559a89937f3
e <>
2020-08-23 23:21:20 +0800

    补充leetcode解题112

--- src/main/java/com/nokia/example/leetcode/tree/TreeTraversal.java
+++ src/main/java/com/nokia/example/leetcode/tree/TreeTraversal.java
@@ -4,6 +4,7 @@ import com.nokia.example.leetcode.entity.NTreeNode;
 import com.nokia.example.leetcode.entity.TreeNode;
 
 import java.util.*;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 /**
  * @author by YingLong on 2020/7/27
@@ -630,4 +631,30 @@ public class TreeTraversal {
         return root;
     }
 
+    /**
+     * 337
+     * 打家劫舍 III
+     *  用 f(o) 表示选择 o 节点的情况下，o 节点的子树上被选择的节点的最大权值和；g(o) 表示不选择 o 节点的情况下，o 节点的子树上被选择的节点的最大权值和；l 和 r 代表 o 的左右孩子。
+     *
+     * 当 o 被选中时，o 的左右孩子都不能被选中，故 o 被选中情况下子树上被选中点的最大权值和为 l 和 r 不被选中的最大权值和相加，即 f(o) = g(l) + g(r)。
+     * 当 o 不被选中时，o 的左右孩子可以被选中，也可以不被选中。对于 o 的某个具体的孩子 xx，它对 o 的贡献是 xx 被选中和不被选中情况下权值和的较大值。故g(o)=max{f(l),g(l)}+max{f(r),g(r)}。
+     *
+     * @param root
+     * @return
+     */
+    public int rob(TreeNode root) {
+        int[] res = robDfs(root);
+        return Math.max(res[0], res[1]);
+    }
+
+    public int[] robDfs(TreeNode root) {
+        if (root == null) {
+            return new int[]{0, 0};
+        }
+        int[] left = robDfs(root.left);
+        int[] rigt = robDfs(root.right);
+        int selected = root.val + left[1] + rigt[1];
+        int notSelected = Math.max(left[0], left[1]) + Math.max(rigt[0], rigt[1]);
+        return new int[]{selected, notSelected};
+    }
 }
