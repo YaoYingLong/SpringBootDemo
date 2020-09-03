fce910904c65e52ec73006a196d9e70721ed3fa0
yao_yinglong <yao_yinglong@icekredit.com>
2020-09-03 18:10:50 +0800

    feat：补充LeetCode算发解题137

--- src/main/java/com/nokia/example/leetcode/tree/BfsTraversal.java
+++ src/main/java/com/nokia/example/leetcode/tree/BfsTraversal.java
@@ -655,6 +655,7 @@ public class BfsTraversal {
         }
         return root;
     }
+
     /**
      * 515
      * 在每个树行中找最大值
@@ -687,4 +688,41 @@ public class BfsTraversal {
         return resultList;
     }
 
+    /**
+     * 1161
+     * 最大层内元素和
+     *
+     * @param root
+     * @return
+     */
+    public int maxLevelSum(TreeNode root) {
+        if (root == null) {
+            return -1;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int minSumLevel = 0;
+        int levelNum = 0;
+        double minSum = Integer.MIN_VALUE;
+        while (!queue.isEmpty()) {
+            int len = queue.size();
+            double levelSum = 0.0;
+            for (int i = 0; i < len; i++) {
+                TreeNode node = queue.poll();
+                levelSum += node.val;
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            levelNum++;
+            if (minSum < levelSum) {
+                minSum = levelSum;
+                minSumLevel = levelNum;
+            }
+        }
+        return minSumLevel;
+    }
 }
