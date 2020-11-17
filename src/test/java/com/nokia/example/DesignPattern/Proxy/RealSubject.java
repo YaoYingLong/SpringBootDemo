5c2898b6c72eb0077eb737fb70a4f9e55d6984a0
yao_yinglong <yao_yinglong@icekredit.com>
2020-11-17 18:19:06 +0800

    feat：补充LeetCode算发解题141

---
+++ src/test/java/com/nokia/example/DesignPattern/Proxy/RealSubject.java
@@ -0,0 +1,11 @@
+package com.nokia.example.DesignPattern.Proxy;
+
+/**
+ * @author by YingLong on 2020/11/5
+ */
+public class RealSubject implements Subject {
+    @Override
+    public void request() {
+        // 业务逻辑处理
+    }
+}
